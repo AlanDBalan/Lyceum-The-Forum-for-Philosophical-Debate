d90369d3bccaf8ab8674b591bb3355fccead1c97
HamzaR19 <132932060+HamzaR19@users.noreply.github.com>
2023-06-29 14:02:19 -0500

    new changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+// router.use('/', homeRoute);
+
+
 router.get('/', (req, res) => {
     res.render('lyceum');
   
