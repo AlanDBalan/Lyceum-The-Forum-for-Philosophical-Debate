147e651a087c2db02fd285081c0f0356d279dcb6
Brian Sellers <bsellers3013@gmail.com>
2023-06-29 13:57:37 -0500

    login appears and working on functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes.js');
+
 router.get('/', (req, res) => {
     res.render('lyceum');
   
 });
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
