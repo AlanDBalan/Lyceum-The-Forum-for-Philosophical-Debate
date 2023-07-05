63c2469b973b1691faf04090be003b95adce5525
HamzaR19 <132932060+HamzaR19@users.noreply.github.com>
2023-07-05 14:32:48 -0500

    made changes to debate

--- db/schema.sql
+++ db/schema.sql
@@ -15,12 +15,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 
-CREATE TABLE IF NOT EXISTS 'philosophy'
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-     school varchar(255) NOT NULL,
-     name varchar(255) NOT NULL,
-     quotes varchar(255) NOT NULL,
-     description varchar(255) NOT NULL,
-        PRIMARY KEY (`id`),
+-- CREATE TABLE IF NOT EXISTS 'philosophy' 
+--     `id` int(11) NOT NULL AUTO_INCREMENT,
+--      school varchar(255) NOT NULL,
+--      name varchar(255) NOT NULL,
+--      quotes varchar(255) NOT NULL,
+--      description varchar(255) NOT NULL,
+--         PRIMARY KEY (`id`),
 
  
